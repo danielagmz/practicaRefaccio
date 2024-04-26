214f8cc293b2cc4ce58be32350f197a4616d361b
danielagmz <161260538+danielagmz@users.noreply.github.com>
2024-04-26 10:44:52 +0200

    refact de clases y eliminar no usados

--- practica_REFAC/src/Order.java
+++ practica_REFAC/src/Order.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Order {
+import java.util.List;
+
+public class Order {
+    private List<OrderLineItem> lineItems;
+    private double taxRate;
+
+    public Order(List<OrderLineItem> lineItems, double taxRate) {
+        this.lineItems = lineItems;
+        this.taxRate = taxRate;
+    }
+
+    public double calculateTotalPrice() {
+        double subtotal = 0.0;
+        for (OrderLineItem item : lineItems) {
+            subtotal += item.getPrice();
+        }
+        double tax = subtotal * taxRate;
+        return subtotal + tax;
+    }
 }
